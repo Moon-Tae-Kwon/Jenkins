d20344a16bfc33788262640530d4de6fc90625fb
Taekwon Moon <m.taekwon.se@gmail.com>
2018-12-11 20:54:17 +0900

    12.11 update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // 앱
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world!!!\n');
+  res.send('Hello auto build test 1\n');
 });
 
 app.listen(PORT, HOST);
